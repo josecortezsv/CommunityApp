6f54bb5632f0c351c8b22d178a2ae2971f1b83cd
josecortezsv <josecortezsv@outlook.com>
2021-07-10 20:45:29 -0600

    Database Initialized and Index Setted

--- PaymentControl/models.py
+++ PaymentControl/models.py
@@ -1,14 +1,12 @@
 import datetime
-
 from django.db import models
-from datetime import datetime, date
-# Create your models here.
-
+from datetime import datetime
+from django.utils import timezone
 
 class Calendarios(models.Model):
     mesanio = models.DateField(unique=True)
-    fechacreacion = models.DateField(default=datetime.now())
-    fechamodificacion = models.DateField(default=datetime.now())
+    fechacreacion = models.DateField(default=timezone.now)
+    fechamodificacion = models.DateField(default=timezone.now)
     usuariocreacion = models.CharField(max_length=30)
     usuarioactualizacion=models.CharField(max_length=30)
 
@@ -17,8 +15,8 @@ class TipoCuotas(models.Model):
     descripcion = models.CharField(max_length=100)
     valor = models.DecimalField(max_digits=6, decimal_places=2)
     inactiva = models.BooleanField(default=False)
-    fechacreacion = models.DateField(default=datetime.now())
-    fechamodificacion = models.DateField(default=datetime.now())
+    fechacreacion = models.DateField(default=timezone.now)
+    fechamodificacion = models.DateField(default=timezone.now)
     usuariocreacion = models.CharField(max_length=30)
     usuarioactualizacion=models.CharField(max_length=30)
 
@@ -27,8 +25,8 @@ class TipoIngresos(models.Model):
     descripcion = models.CharField(max_length=100)
     codigo = models.CharField(max_length=100)
     inactiva = models.BooleanField(default=False)
-    fechacreacion = models.DateField(default=datetime.now())
-    fechamodificacion = models.DateField(default=datetime.now())
+    fechacreacion = models.DateField(default=timezone.now)
+    fechamodificacion = models.DateField(default=timezone.now)
     usuariocreacion = models.CharField(max_length=30)
     usuarioactualizacion=models.CharField(max_length=30)
 
@@ -37,8 +35,8 @@ class TipoEgresos(models.Model):
     descripcion = models.CharField(max_length=100)
     codigo = models.CharField(max_length=100)
     inactiva = models.BooleanField(default=False)
-    fechacreacion = models.DateField(default=datetime.now())
-    fechamodificacion = models.DateField(default=datetime.now())
+    fechacreacion = models.DateField(default=timezone.now)
+    fechamodificacion = models.DateField(default=timezone.now)
     usuariocreacion = models.CharField(max_length=30)
     usuarioactualizacion=models.CharField(max_length=30)
 
@@ -46,8 +44,8 @@ class TipoEgresos(models.Model):
 class TipoInmuebles(models.Model):
     descripcion = models.CharField(max_length=100)
     inactiva = models.BooleanField(default=False)
-    fechacreacion = models.DateField(default=datetime.now())
-    fechamodificacion = models.DateField(default=datetime.now())
+    fechacreacion = models.DateField(default=timezone.now)
+    fechamodificacion = models.DateField(default=timezone.now)
     usuariocreacion = models.CharField(max_length=30)
     usuarioactualizacion=models.CharField(max_length=30)
 
@@ -55,8 +53,8 @@ class TipoInmuebles(models.Model):
 class TipoPagos(models.Model):
     descripcion = models.CharField(max_length=100)
     inactiva = models.BooleanField(default=False)
-    fechacreacion = models.DateField(default=datetime.now())
-    fechamodificacion = models.DateField(default=datetime.now())
+    fechacreacion = models.DateField(default=timezone.now)
+    fechamodificacion = models.DateField(default=timezone.now)
     usuariocreacion = models.CharField(max_length=30)
     usuarioactualizacion=models.CharField(max_length=30)
 
@@ -76,8 +74,8 @@ class CuentasBancarias(models.Model):
     principal = models.BooleanField()
     comentario = models.CharField(max_length=250)
     inactiva = models.BooleanField(default=False)
-    fechacreacion = models.DateField(default=datetime.now())
-    fechamodificacion = models.DateField(default=datetime.now())
+    fechacreacion = models.DateField(default=timezone.now)
+    fechamodificacion = models.DateField(default=timezone.now)
     usuariocreacion = models.CharField(max_length=30)
     usuarioactualizacion=models.CharField(max_length=30)
 
@@ -91,8 +89,8 @@ class Proyectos(models.Model):
     numerocuotas = models.IntegerField()
     cuotaasociada = models.IntegerField()
     inactiva = models.BooleanField(default=False)
-    fechacreacion = models.DateField(default=datetime.now())
-    fechamodificacion = models.DateField(default=datetime.now())
+    fechacreacion = models.DateField(default=timezone.now)
+    fechamodificacion = models.DateField(default=timezone.now)
     usuariocreacion = models.CharField(max_length=30)
     usuarioactualizacion=models.CharField(max_length=30)
 
@@ -105,8 +103,8 @@ class Personas(models.Model):
     correo = models.CharField(max_length=250)
     correoalterno = models.CharField(max_length=250)
     inactivo = models.BooleanField(default=False)
-    fechacreacion = models.DateField(default=datetime.now())
-    fechamodificacion = models.DateField(default=datetime.now())
+    fechacreacion = models.DateField(default=timezone.now)
+    fechamodificacion = models.DateField(default=timezone.now)
     usuariocreacion = models.CharField(max_length=30)
     usuarioactualizacion=models.CharField(max_length=30)
 
@@ -118,8 +116,8 @@ class Proveedores(models.Model):
     direccion = models.CharField(max_length=250)
     nit = models.CharField(max_length=50, unique=True)
     comentarios = models.TextField()
-    fechacreacion = models.DateField(default=datetime.now())
-    fechamodificacion = models.DateField(default=datetime.now())
+    fechacreacion = models.DateField(default=timezone.now)
+    fechamodificacion = models.DateField(default=timezone.now)
     usuariocreacion = models.CharField(max_length=30)
     usuarioactualizacion=models.CharField(max_length=30)
 
@@ -131,14 +129,14 @@ class Inmuebles(models.Model):
     tipocuota = models.ForeignKey(TipoCuotas, on_delete=models.PROTECT)
     propietario = models.ForeignKey(Personas, on_delete=models.PROTECT)
     propietario_desde = models.DateField()
-    fechacreacion = models.DateField(default=datetime.now())
-    fechamodificacion = models.DateField(default=datetime.now())
+    fechacreacion = models.DateField(default=timezone.now)
+    fechamodificacion = models.DateField(default=timezone.now)
     usuariocreacion = models.CharField(max_length=30)
     usuarioactualizacion=models.CharField(max_length=30)
 
 
 class Ingresos(models.Model):
-    fecha = models.DateField(default=datetime.today())
+    fecha = models.DateField(default=timezone.now)
     mesanio = models.ForeignKey(Calendarios, on_delete=models.PROTECT)
     tipoingreso = models.ForeignKey(TipoIngresos, on_delete=models.PROTECT)
     tipopago = models.ForeignKey(TipoPagos, on_delete=models.PROTECT)
@@ -151,14 +149,14 @@ class Ingresos(models.Model):
     total = models.DecimalField(max_digits=10, decimal_places=2)
     comentario = models.TextField()
     anulado = models.BooleanField(default=False)
-    fechacreacion = models.DateField(default=datetime.now())
-    fechamodificacion = models.DateField(default=datetime.now())
+    fechacreacion = models.DateField(default=timezone.now)
+    fechamodificacion = models.DateField(default=timezone.now)
     usuariocreacion = models.CharField(max_length=30)
     usuarioactualizacion=models.CharField(max_length=30)
 
 
 class Egresos(models.Model):
-    fecha = models.DateField(default=datetime.today())
+    fecha = models.DateField(default=timezone.now)
     numerodocumento = models.DateField(max_length=50)
     FACTURA='01'
     RECIBO='02'
@@ -177,8 +175,8 @@ class Egresos(models.Model):
     anulado = models.BooleanField(default=False)
     comentarios = models.TextField()
     proyecto = models.ForeignKey(Proyectos, on_delete=models.PROTECT)
-    fechacreacion = models.DateField(default=datetime.now())
-    fechamodificacion = models.DateField(default=datetime.now())
+    fechacreacion = models.DateField(default=timezone.now)
+    fechamodificacion = models.DateField(default=timezone.now)
     usuariocreacion = models.CharField(max_length=30)
     usuarioactualizacion=models.CharField(max_length=30)
 
